44bf1b8ca1a9256ca242118dccf0c22c6cd02f52
Maximilian Tagher <feedback.tagher@gmail.com>
2013-01-03 08:57:44 -0800

    Initial Commit

---
+++ AnimationDemonstration/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AnimationDemonstration
+//
+//  Created by Maximilian Tagher on 1/3/13.
+//  Copyright (c) 2013 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AnimationDemonstration/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AnimationDemonstration
+//
+//  Created by Maximilian Tagher on 1/3/13.
+//  Copyright (c) 2013 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
